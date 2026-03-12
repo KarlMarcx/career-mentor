86dd636091cf6494f876d3923f8774908b0f032e
KarlMarcx <k.bardago.550158@umindanao.edu.ph>
2026-03-12 20:16:44 +0800

    change model

--- tools.py
+++ tools.py
@@ -8,10 +8,8 @@ client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 def ask_llm(prompt):
 
     response = client.chat.completions.create(
-        model="llama3-70b-8192",
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
+        model="llama-3.1-8b-instant",
+        messages=[{"role": "user", "content": prompt}]
     )
 
     return response.choices[0].message.content
